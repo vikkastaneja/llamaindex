e7b8024430e2da7767236f522ca8a6e384ac6ee9
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-04-09 00:04:47 -0700

    Querying - adding callback manager
    In this step, we have added a callback manager that actually traces the time taken by each steps such as below:
    **********
    Trace: index_construction
    **********
    **********
    Trace: query
        |_query -> 1.914332 seconds
          |_retrieve -> 0.698518 seconds
            |_embedding -> 0.335219 seconds
          |_synthesize -> 1.214466 seconds
            |_templating -> 2.9e-05 seconds
            |_llm -> 1.203278 seconds
    **********

--- documentation-helper-llamaindex/main.py
+++ documentation-helper-llamaindex/main.py
@@ -1,15 +1,21 @@
 import os
 from dotenv import load_dotenv
+from llama_index.core.callbacks import LlamaDebugHandler, CallbackManager
+
 load_dotenv()
 
 from pinecone import Pinecone
-from llama_index.core import VectorStoreIndex
+from llama_index.core import VectorStoreIndex, Settings
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 
 if __name__ == '__main__':
     pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
     pinecone_index = pc.Index(name=os.getenv('PINECONE_INDEX'))
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+
+    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+    callback_manager = CallbackManager(handlers=[llama_debug])
+    Settings.callback_manager = callback_manager
 
     index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
     query = 'What is a llamaindex query engine?'
