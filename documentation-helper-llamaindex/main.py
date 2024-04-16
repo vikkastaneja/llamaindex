46b3efe1f3f954f748e5bf9e9c992f3f85258240
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-04-15 22:00:51 -0700

    Basic chatbot using streamlit and llamaindex's chat_engine
    This change introduces chat_engine from llamaindex to create a chat session and then uses streamlit to create interactive UI for conversational AI that remembers context from that session.

--- documentation-helper-llamaindex/main.py
+++ documentation-helper-llamaindex/main.py
@@ -1,24 +1,66 @@
 import os
 from dotenv import load_dotenv
 from llama_index.core.callbacks import LlamaDebugHandler, CallbackManager
+from llama_index.core.chat_engine.types import ChatMode
 
 load_dotenv()
-
+import streamlit as st
 from pinecone import Pinecone
 from llama_index.core import VectorStoreIndex, Settings
 from llama_index.vector_stores.pinecone import PineconeVectorStore
 
-if __name__ == '__main__':
+
+@st.cache_resource(show_spinner=False)
+def get_index() -> VectorStoreIndex:
     pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-    pinecone_index = pc.Index(name=os.getenv('PINECONE_INDEX'))
+    pinecone_index = pc.Index(name=os.getenv("PINECONE_INDEX"))
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 
     llama_debug = LlamaDebugHandler(print_trace_on_end=True)
     callback_manager = CallbackManager(handlers=[llama_debug])
     Settings.callback_manager = callback_manager
 
-    index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
-    query = 'What is a llamaindex query engine?'
-    query_engine = index.as_query_engine()
-    response = query_engine.query(query)
-    print(response)
+    return VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+
+index = get_index()
+if "chat_engine" not in st.session_state.keys():
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode=ChatMode.CONTEXT, verbose=True
+    )
+
+
+st.set_page_config(
+    page_title="Chat with Llamaindex docs, powered by Llama Index",
+    layout="centered",
+    page_icon="🦙",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+st.title("Chat with llamaindex docs, 🦙")
+
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about LlamaIndex's open source python library",
+        }
+    ]
+
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if st.session_state.messages[-1]["role"] == "user":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(message=prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+
+            st.session_state.messages.append(message)
